436e8dec147bad797404867a5020cf4efb4369c0
Katie Otto <33523737+kateotto@users.noreply.github.com>
2020-04-16 19:37:42 +0200

    Updates

---
+++ precache-manifest.ec1952dcd2088b0cac493c9e0324b38e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dea0a34321e071f9568b7ba2bc40973",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "4e8ee080bf9d201fdf46",
+    "url": "/pokedex/static/css/main.749bc96a.chunk.css"
+  },
+  {
+    "revision": "483f314e89511d67f03b",
+    "url": "/pokedex/static/js/2.5badfe40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.5badfe40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8ee080bf9d201fdf46",
+    "url": "/pokedex/static/js/main.31285844.chunk.js"
+  },
+  {
+    "revision": "23e4c6896b4e85d076c4",
+    "url": "/pokedex/static/js/runtime-main.3f186e6a.js"
+  },
+  {
+    "revision": "6e5393a64ca23c455e8568d409e82da6",
+    "url": "/pokedex/static/media/Pokemon_Hollow.6e5393a6.ttf"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/pokedex/static/media/Pokemon_Solid.36d26dbc.ttf"
+  },
+  {
+    "revision": "01e51eaebfca712bf731c79ed667b65c",
+    "url": "/pokedex/static/media/main-pattern.01e51eae.png"
+  }
+]);
